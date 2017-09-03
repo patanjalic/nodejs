a98c0e7acc32b2f7a32642ea7452ff747bf87ba1
Patanjali C <patanjalic@github.com>
2017-09-03 19:57:02 -0400

    third party modules

--- n-3-2-module-basics/app.js
+++ n-3-2-module-basics/app.js
@@ -3,11 +3,18 @@ console.log('Starting app.');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
-
+const _ = require('lodash');
 var user = os.userInfo();
 
-var note  = notes.addNote();
-console.log(note);
-console.log(notes.add(9,-2));
+// var note  = notes.addNote();
+// console.log(note);
+// console.log(notes.add(9,-2));
 // console.log(user.username);
 // fs.appendFile('greetings.txt',`Hello ${user.username}!`);
+
+
+console.log(_.isString(true));
+console.log(_.isString('javascript'));
+
+var array = [1,2,3,4,1,2,5,8,9,10];
+console.log(_.uniq(array));
