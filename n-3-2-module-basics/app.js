95c0692337509acb717c98218fe8bc527afca009
Patanjali C <patanjalic@github.com>
2017-09-03 19:32:26 -0400

    using user defined module and inbuilt modules

--- n-3-2-module-basics/app.js
+++ n-3-2-module-basics/app.js
@@ -2,7 +2,12 @@ console.log('Starting app.');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
 var user = os.userInfo();
-console.log(user.username);
-fs.appendFile('greetings.txt',`Hello ${user.username}!`);
+
+var note  = notes.addNote();
+console.log(note);
+console.log(notes.add(9,-2));
+// console.log(user.username);
+// fs.appendFile('greetings.txt',`Hello ${user.username}!`);
---
+++ n-3-2-module-basics/notes.js
@@ -0,0 +1,11 @@
+console.log('starting notes');
+
+module.exports.addNote = () => {
+  console.log('addnode');
+  return 'new note';
+};
+
+module.exports.add = (a,b) => {
+  console.log('add');
+  return a + b;
+};
