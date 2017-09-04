a17cf69a724bc7577670de684fa523c3761ec0ed
Patanjali C <patanjalic@github.com>
2017-09-03 20:05:13 -0400

    nodemon

--- n-3-2-module-basics/app.js
+++ n-3-2-module-basics/app.js
@@ -16,5 +16,5 @@ var user = os.userInfo();
 console.log(_.isString(true));
 console.log(_.isString('javascript'));
 
-var array = [1,2,3,4,1,2,5,8,9,10];
+var array = [1,2,3,4,1,2,5,8,9,11,100];
 console.log(_.uniq(array));
