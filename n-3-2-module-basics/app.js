d08bd08c18289188a135effaeb8e2e3813a66f74
Patanjali C <patanjalic@github.com>
2017-09-03 20:30:40 -0400

    input arguments

--- n-3-2-module-basics/app.js
+++ n-3-2-module-basics/app.js
@@ -1,20 +1,20 @@
 console.log('Starting app.');
 
+
 const fs = require('fs');
-const os = require('os');
-const notes = require('./notes.js');
 const _ = require('lodash');
-var user = os.userInfo();
 
-// var note  = notes.addNote();
-// console.log(note);
-// console.log(notes.add(9,-2));
-// console.log(user.username);
-// fs.appendFile('greetings.txt',`Hello ${user.username}!`);
+const notes = require('./notes.js');
 
-
-console.log(_.isString(true));
-console.log(_.isString('javascript'));
-
-var array = [1,2,3,4,1,2,5,8,9,11,100];
-console.log(_.uniq(array));
+const command = process.argv[2];
+if(command === 'add') {
+  console.log('adding new note');
+} else if(command === 'list') {
+  console.log('listing notes');
+} else if(command === 'remove'){
+  console.log('remove notes');
+} else if(command === 'read'){
+  console.log('reading note');
+}else {
+  console.log('command not valid');
+}
