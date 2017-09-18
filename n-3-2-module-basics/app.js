0523b7a7b5c6bf5ca2555199377bee6a30f7c246
Patanjali C <patanjalic@github.com>
2017-09-18 06:34:35 -0400

    list notes

--- n-3-2-module-basics/app.js
+++ n-3-2-module-basics/app.js
@@ -3,18 +3,32 @@ console.log('Starting app.');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const command = process.argv[2];
+
+const command = yargs.argv._[0];
+console.log(process.argv);
+console.log(yargs.argv);
 if(command === 'add') {
-  console.log('adding new note');
+  var note = notes.addNote(yargs.argv.title,yargs.argv.body);
+  if(note != undefined) {
+    console.log(`note added successfully ${note.title} ${note.body}`);
+  } else {
+    console.log('note already exists');
+  }
 } else if(command === 'list') {
-  console.log('listing notes');
+  var allNotes = notes.listAll();
+  allNotes.forEach((note) => notes.logNote(note));
 } else if(command === 'remove'){
-  console.log('remove notes');
+  var isRemoved = notes.removeNote(yargs.argv.title);
+  var message = isRemoved?'Note is removed':'Note is not removed';
+  console.log(message);
 } else if(command === 'read'){
-  console.log('reading note');
+  var note = notes.readNote(yargs.argv.title);
+  var message = (note != undefined) ? `Note found ${note.title} ${note.body}`:'Note not found';
+  console.log(message);
 }else {
   console.log('command not valid');
 }
--- n-3-2-module-basics/notes.js
+++ n-3-2-module-basics/notes.js
@@ -1,11 +1,59 @@
 console.log('starting notes');
+const fs = require('fs');
 
-module.exports.addNote = () => {
-  console.log('addnode');
-  return 'new note';
+var fetchNotes = () => {
+  try {
+      var notesString = fs.readFileSync('notes-data.json');
+      return JSON.parse(notesString);
+  } catch(e) {
+      return [];
+  }
 };
 
-module.exports.add = (a,b) => {
-  console.log('add');
-  return a + b;
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json',JSON.stringify(notes));
+};
+
+var addNote = (title,body) => {
+  console.log('adding note',title,body);
+  var notes = fetchNotes();
+  note = {title,body};
+
+  var duplicate = notes.filter((note) => note.title === title);
+  if(duplicate.length === 0) {
+    notes.push(note);
+    saveNotes(notes);
+    return note;
+  }
+};
+
+var logNote = (note) => {
+  console.log(`Title: ${note.title} Body: ${note.body}`);
+};
+
+var listAll = () => {
+  return fetchNotes();
+};
+
+var readNote = (title) => {
+  var notes = fetchNotes();
+  var note = notes.filter((note) => note.title === title);
+  console.log(note[0]);
+  return note[0];
+};
+
+var removeNote = (title) => {
+  var notes = fetchNotes();
+  var updatedNotes = notes.filter((note) => note.title !== title);
+  saveNotes(updatedNotes);
+  return notes.length !== updatedNotes.length;
+};
+
+
+module.exports = {
+  addNote,
+  listAll,
+  readNote,
+  removeNote,
+  logNote
 };
